1247f6a1b0accb3e8d6fabe152a10fb736f1c739
Martin Kealey <martin@kurahaupo.gen.nz>
2023-11-05 10:40:46 +1000

    Additional novel implementation of 42

--- 42.c
+++ 42.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
+#if defined ORIGINAL
+/* retained for literary criticism */
+#define SIX 1+5
+#define MINE 8+1
+#elif ! defined INSANE
+/* a sensible reimplementation */
 #define SIX 3+3
 #define NINE 15-6
+#else
+/* a less sensible reimplementation */
+#define SIX 24-18
+#define NINE 1/-2+9
+#endif
 
 int main(void)
 {
